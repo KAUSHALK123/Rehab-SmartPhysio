d3298c606cc007057f4ddf46ca7e5dc54e7f655b
kaushalk123 <kaushalk123000@gmail.com>
2026-08-09 12:13:12 +0530

    Lock camera angle and zoom out elbow model

--- frontend/src/components/ElbowCalibrationViewer.jsx
+++ frontend/src/components/ElbowCalibrationViewer.jsx
@@ -5,7 +5,7 @@ import * as THREE from 'three';
 
 function ElbowModel({ elbowAngle }) {
   const { scene, nodes } = useGLTF('/models/elbow.glb');
-  
+
   // Forearm and hand node in the elbow.glb model
   const forearmNode = nodes['WristArm'];
 
@@ -29,8 +29,8 @@ function ElbowModel({ elbowAngle }) {
     if (forearmNode) {
       // Bends from 180° (fully extended, straight) to 90° (fully flexed, bent)
       // Map 180 -> 0 degrees, 90 -> 90 degrees bend
-      const bendDeg = 180 - elbowAngle; 
-      
+      const bendDeg = 180 - elbowAngle;
+
       // Bending rotation around local Y-axis (positive to bend forward)
       const targetRotation = THREE.MathUtils.degToRad(bendDeg);
 
@@ -44,7 +44,7 @@ function ElbowModel({ elbowAngle }) {
 
   return (
     // Rotate model Y-axis by 120 degrees to show the bicep clearly
-    <group rotation={[0, (120 * Math.PI) / 180, 0]} scale={[1.3, 1.3, 1.3]} position={[0, -0.2, 0]}>
+    <group rotation={[0, (180 * Math.PI) / 180, 0]} scale={[1.3, 1.3, 1.3]} position={[0, -0.2, 0]}>
       <primitive object={scene} />
     </group>
   );
@@ -54,18 +54,16 @@ export default function ElbowCalibrationViewer({ elbowAngle = 180 }) {
   return (
     <div className="w-full h-full relative select-none">
       <Canvas
-        camera={{ position: [0, 0, 2.8], fov: 35 }}
+        camera={{ position: [0, 0, 3.2], fov: 35 }}
         gl={{ antialias: true, alpha: true }}
       >
         <ambientLight intensity={0.7} />
         <directionalLight position={[5, 5, 5]} intensity={1.2} />
         <directionalLight position={[-5, -5, -5]} intensity={0.5} />
-        
+
         <Center>
           <ElbowModel elbowAngle={elbowAngle} />
         </Center>
-        
-        <OrbitControls enableZoom={true} enablePan={false} maxDistance={4} minDistance={1.0} />
       </Canvas>
     </div>
   );
